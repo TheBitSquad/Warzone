f87de8ac00c36886669f883cc84426f3ddd0fbd0
amansahu278 <a.man.cubes24@gmail.com>
2023-09-26 13:33:00 -0400

    chore: Renamed Main.java

---
+++ src/main/java/org/bitsquad/warzone/Main.java
@@ -0,0 +1,10 @@
+package org.bitsquad.warzone;
+
+/**
+ * Main Class
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Hello and welcome!");
+    }
+}

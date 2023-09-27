89fcaec2b6abd86728851c3652bba1380e34e53e
unknown <nisargadalja24680@gmail.com>
2023-09-27 19:36:47 -0400

    updated country

--- src/main/java/org/bitsquad/warzone/country/Country.java
+++ src/main/java/org/bitsquad/warzone/country/Country.java
@@ -6,6 +6,186 @@ package org.bitsquad.warzone.country;
  * This class defines a country with its ID, list of neighbours and other relevant details according to system design.
  */
 public class Country {
+    /**
+     * The id of the country.
+     */
+    private int d_countryId;
 
+    /**
+     * The name of the country.
+     */
+    private String d_countryName;
+
+    /**
+     * noOfArmies stores the number of Armies allocated to the country by the owner.
+     */
+    private int d_noOfArmies;
+
+    /**
+     * The id of the continent to which the country belongs.
+     */
+    private int d_continentId;
+
+    /**
+     * The player id who owns the country.
+     */
+    private int d_ownedByPlayerId;
+
+    /**
+     * d_neighbors stores the list of neighbor countries' id.
+     */
+    private ArrayList<Integer> d_neighbors;
+
+
+    Country(){
+        this.d_countryId = 0;
+        this.d_countryName = "n";
+        this.d_noOfArmies= 0;
+        this.d_continentId= 0;
+        this.d_ownedByPlayerId = 0;
+        this.d_neighbors = new ArrayList<Integer>();
+    }
+    Country(int p_countryId,String p_countryName,int p_noOfArmies,int p_continentId,int p_ownedByPlayerId,ArrayList<Integer> p_neighbors){
+        this.d_countryId = p_countryId;
+        this.d_countryName = p_countryName;
+        this.d_noOfArmies= p_noOfArmies;
+        this.d_continentId= p_continentId;
+        this.d_ownedByPlayerId = p_ownedByPlayerId;
+        this.d_neighbors = p_neighbors;
+    }
+
+
+    /**
+     *  getter method of Country Id.
+     * @return the id of the country
+     */
+    public int getCountryId() {
+        return d_countryId;
+    }
+
+    /**
+     * setId method will set the id of the Country.
+     * @param p_id the id of country.
+     */
+    public void setCountryId(int p_countryId) {
+        if (p_countryId>=1) {
+            this.d_countryId = p_countryId;
+        }
+    }
+
+
+    /**
+     * getter method for Country name
+     * @return the country name.
+     */
+    public String getCountryName() {
+        return d_countryName;
+    }
+
+    /**
+     * setter method for country name.
+     * @param p_countryName
+     */
+    public void setCountryName(String p_countryName) {
+        this.d_countryName = p_countryName;
+    }
+
+
+    /**
+     * Getter method of NoOfArmies which get number of Country's Armies.
+     * @return the number of Armies allocated to the country by the owner.
+     */
+    public int getNoOfArmies() {
+        return d_noOfArmies;
+    }
+
+    /**
+     * Setter method of noOfArmies.
+     * @param p_noOfArmies the number of Country's Armies.
+     */
+    public void setNoOfArmies(int p_noOfArmies) {
+        this.d_noOfArmies = p_noOfArmies;
+    }
+
+
+    /**
+     * Getter method of continent Id.
+     * @return the continent id.
+     */
+    public int getContinentId() {
+        return d_continentId;
+    }
+
+    /**
+     * Setter method of continentId which set the continent id.
+     * @param p_continentId the continent id.
+     */
+    public void setContinentId(int p_continentId) {
+        this.d_continentId = p_continentId;
+    }
+
+
+    /**
+     * This getter method use to get the player id who owns the country.
+     * @return the player id.
+     */
+    public int getOwnedByPlayerId() {
+        return d_ownedByPlayerId;
+    }
+
+    /**
+     * This setter method use to set the Player Id who owns the country.
+     * @param p_ownedByPlayerId the Owned player id.
+     */
+    public void setOwnedByPlayerId(int p_ownedByPlayerId) {
+        this.d_ownedByPlayerId = p_ownedByPlayerId;
+    }
+
+
+    /**
+     * This getter method use to get the list of neighbor Countries.
+     * @return the ArrayList of neighbor Countries
+     */
+    public ArrayList<Integer> getNeighbors() {
+        return d_neighbors;
+    }
+
+    /**
+     * This method use to set the list of neighbor countries.
+     * @param p_neighbors the integer ArrayList of neighbor countries's Id.
+     */
+    public void setNeighbors(ArrayList<Integer> p_neighbors) {
+        this.d_neighbors = p_neighbors;
+    }
+
+    /**
+     *  addNeighbor method add the id of the neighbor country in the neighbors list.
+     * @param p_neighborCountryId the id of the neighbor.
+     */
+    public boolean addNeighbor(int p_neighborCountryId) {
+        if (d_neighbors.contains(p_neighborCountryId) == false) {
+            d_neighbors.add(p_neighborCountryId);
+            return true;
+        }
+        else {
+            System.out.println("The Neighbor Country is already is added");
+            return false;
+        }
+
+    }
+
+    /**
+     * removeNeighbor method remove the id of the neighbor country from the neighbors list.
+     * @param p_neighborCountryId the id of the neighbor country.
+     */
+    public boolean removeNeighbor(int p_neighborCountryId) {
+        if(p_neighborCountryId>=1 && d_neighbors.contains(p_neighborCountryId)) {
+            d_neighbors.remove(Integer.valueOf(p_neighborCountryId));
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 
 }
--- src/test/java/org/bitsquad/warzone/country/CountryTest.java
+++ src/test/java/org/bitsquad/warzone/country/CountryTest.java
@@ -1,6 +1,8 @@
 package org.bitsquad.warzone.country;
-
+import java.util.ArrayList;
+import org.junit.jupiter.api.BeforeAll;
 import static org.junit.jupiter.api.Assertions.*;
+
 
 import org.junit.jupiter.api.Test;
 
@@ -9,8 +11,31 @@ import org.junit.jupiter.api.Test;
  */
 class CountryTest {
 
+	ArrayList<Integer> ns= new ArrayList<Integer>();
+	private final Country countryObject = new Country(1,"India",4,5,4,ns);
+
+	/**
+	 * This method will testing that neighbor country id is only be added if it is not added.
+	 * if id is already added in neighbor list then the test case is fail
+	 */
 	@Test
-	void test() {
+	void testAddNeighbor() {
+		if(countryObject.getNeighbors().contains(1) == false) {
+			System.out.println(countryObject.addNeighbor(1));
+		}
+		else {
+			fail("test should have been aborted because country is already added in neighbors list");
+		}
+	}
+
+	@Test
+	void testRemoveNeighbor() {
+		if(countryObject.getNeighbors().contains(2)== true) {
+			fail("test should have been aborted becuase country you want to reomve is not present in neighbor list");
+		}
+		else {
+			System.out.println(countryObject.removeNeighbor(1));
+		}
 	}
 
 }

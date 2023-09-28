6ed64d3359f9499719259b6bd8bf6e6072b12bf7
Sriluharshini <sriluharshini@gmail.com>
2023-09-28 18:42:07 -0400

    Order class implementation

--- src/main/java/org/bitsquad/warzone/order/Order.java
+++ src/main/java/org/bitsquad/warzone/order/Order.java
@@ -6,6 +6,7 @@ package org.bitsquad.warzone.order;
  * This class defines the order placed with the ID of the player, the source and
  * target countries, the number of army units involved in the order
  * and the type of the action as per game-play actions.
+ * 
  */
 public class Order {
     public int d_playerID;
@@ -18,6 +19,15 @@ public class Order {
         DEPLOY, ADVANCE, BOMB, BLOCKADE, AIRLIFT, NEGOTIATE
     };
 
+    /**
+     * 
+     * @param p_playerID        Player ID
+     * @param p_sourceCountryID Source Country ID
+     * @param p_targetCountryID Target Country ID
+     * @param p_noOfArmyUnits   No of Army Units
+     * @param p_typeOfAction    Type of Actions
+     */
+
     public Order(int p_playerID, int p_sourceCountryID, int p_targetCountryID, int p_noOfArmyUnits,
             TYPEOFACTION p_typeOfAction) {
         this.d_playerID = p_playerID;
@@ -27,35 +37,66 @@ public class Order {
         this.d_typeOfAction = p_typeOfAction;
     }
 
-    // Setters and getters
+    /**
+     * 
+     * @param p_playerID Setting the player ID
+     */
     public void setPlayerID(int p_playerID) {
         d_playerID = p_playerID;
     }
 
+    /**
+     * 
+     * @return Returns the player ID
+     */
     public int getPlayerID() {
         return d_playerID;
     }
 
+    /**
+     * 
+     * @param p_sourceCountryID Setting the source country ID
+     */
     public void setSourceCountryID(int p_sourceCountryID) {
         d_sourceCountryID = p_sourceCountryID;
     }
 
-    public int getSourceCountryID(int p_sourceCountryID) {
+    /**
+     * 
+     * @return Returns the source country ID
+     */
+    public int getSourceCountryID() {
         return d_sourceCountryID;
     }
 
+    /**
+     * 
+     * @param p_targetCountryID Setting the target country ID
+     */
     public void setTargetCountryID(int p_targetCountryID) {
         d_targetCountryID = p_targetCountryID;
     }
 
-    public int setTargetCountryID() {
+    /**
+     * 
+     * @return Returns the target country ID
+     */
+    public int getTargetCountryID() {
         return d_targetCountryID;
     }
 
+    /**
+     * 
+     * @param p_noOfArmyUnits Setting the number of army units
+     */
     public void setNoOfArmyUnits(int p_noOfArmyUnits) {
         d_noOfArmyUnits = p_noOfArmyUnits;
     }
 
+    /**
+     * 
+     * @return Returns the number of army units
+     */
     public int getNoOfArmyUnits() {
         return d_noOfArmyUnits;
     }

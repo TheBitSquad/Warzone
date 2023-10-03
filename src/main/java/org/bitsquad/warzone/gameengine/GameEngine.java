ec5d7c46fd52e68860490c5ba64db33a4038520d
amansahu278 <a.man.cubes24@gmail.com>
2023-10-02 20:41:44 -0400

    Added GameEngine singleton class

--- src/main/java/org/bitsquad/warzone/gameengine/GameEngine.java
+++ src/main/java/org/bitsquad/warzone/gameengine/GameEngine.java
@@ -1,9 +1,52 @@
 package org.bitsquad.warzone.gameengine;
 
+import org.bitsquad.warzone.cli.CliParser;
+import org.bitsquad.warzone.cli.CliResponse;
+import org.bitsquad.warzone.map.Map;
+import org.bitsquad.warzone.player.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 /**
  * Represents the Game engine.
  * 
  * This class defines a game with a map and a list of players, along with execution details and methods.
  */
 public class GameEngine {
+    private static GameEngine d_instance;
+
+    Map d_gameMap;
+    List<Player> d_gamePlayers;
+    GameEngine(){
+        d_gameMap = new Map();
+        d_gamePlayers = new ArrayList<>();
+    }
+
+    public static GameEngine get_instance(){
+        if(d_instance == null) {
+            d_instance = new GameEngine();
+        }
+        return d_instance;
+    }
+
+    public void printNumberOfPlayers() {
+        System.out.println(this.d_gamePlayers.size());
+    }
+    public static void main(String[] args) throws ClassNotFoundException {
+        CliParser parser = new CliParser();
+        Scanner scanner = new Scanner(System.in);
+
+        String ip;
+        while(true){
+            ip = scanner.nextLine();
+            try{
+                CliResponse resp = parser.parseCommandString(ip);
+            } catch (Exception e){
+                System.err.println(e.getMessage());
+            }
+        }
+    }
+
 }

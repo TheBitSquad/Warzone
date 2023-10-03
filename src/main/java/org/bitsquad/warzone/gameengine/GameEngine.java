22a84512993b3a007a5dbe98617b78f06599e699
Amin-MAG <M.Amin.Ghasvari@Gmail.com>
2023-10-03 04:44:31 +0330

    add state and getter, setters to game engine

--- src/main/java/org/bitsquad/warzone/gameengine/GameEngine.java
+++ src/main/java/org/bitsquad/warzone/gameengine/GameEngine.java
@@ -11,21 +11,49 @@ import java.util.Scanner;
 
 /**
  * Represents the Game engine.
- * 
+ * <p>
  * This class defines a game with a map and a list of players, along with execution details and methods.
  */
 public class GameEngine {
     private static GameEngine d_instance;
-    public enum PHASE{DEFAULT, SETUP, STARTUP, ORDER}
-    Map d_gameMap;
-    List<Player> d_gamePlayers;
-    GameEngine(){
+
+    public enum PHASE {DEFAULT, SETUP, STARTUP, ORDER}
+
+    private Map d_gameMap;
+    private List<Player> d_gamePlayers;
+    private PHASE d_currentPhase;
+
+    public Map getGameMap() {
+        return d_gameMap;
+    }
+
+    public void setGameMap(Map p_gameMap) {
+        this.d_gameMap = p_gameMap;
+    }
+
+    public List<Player> getGamePlayers() {
+        return d_gamePlayers;
+    }
+
+    public void setGamePlayers(List<Player> p_gamePlayers) {
+        this.d_gamePlayers = p_gamePlayers;
+    }
+
+    public PHASE getCurrentPhase() {
+        return d_currentPhase;
+    }
+
+    public void setCurrentPhase(PHASE p_currentPhase) {
+        this.d_currentPhase = p_currentPhase;
+    }
+
+    GameEngine() {
         d_gameMap = new Map();
         d_gamePlayers = new ArrayList<>();
     }
 
-    public static GameEngine get_instance(){
-        if(d_instance == null) {
+    public static GameEngine get_instance() {
+        if (d_instance == null) {
             d_instance = new GameEngine();
         }
         return d_instance;
@@ -36,11 +64,11 @@ public class GameEngine {
         Scanner scanner = new Scanner(System.in);
 
         String ip;
-        while(true){
+        while (true) {
             ip = scanner.nextLine();
-            try{
+            try {
                 CliResponse resp = parser.parseCommandString(ip);
-            } catch (Exception e){
+            } catch (Exception e) {
                 System.err.println(e.getMessage());
             }
         }

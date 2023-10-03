9f843ff99d10b2f89c1b2bde3817907e4da3906b
Amin-MAG <M.Amin.Ghasvari@Gmail.com>
2023-10-03 05:58:54 +0330

    style: update the Commands.java formatting

--- src/main/java/org/bitsquad/warzone/cli/Commands.java
+++ src/main/java/org/bitsquad/warzone/cli/Commands.java
@@ -1,5 +1,5 @@
 /**
- *  This file contains all the commands which are represented each in their own class, implemented with the help of picocli. 
+ * This file contains all the commands which are represented each in their own class, implemented with the help of picocli.
  */
 package org.bitsquad.warzone.cli;
 
@@ -15,27 +15,28 @@ import picocli.CommandLine.Spec;
 import java.util.concurrent.Callable;
 
 @Command(name = "editcontinent", mixinStandardHelpOptions = true, version = "1.0.0")
-class EditContinent implements Callable<Integer>{
+class EditContinent implements Callable<Integer> {
     @Option(names = "-add", arity = "2", paramLabel = "CONTINENT_ID VALUE",
             description = "Enter continent and value respectively")
     int[] d_addArray;
     @Option(names = "-remove", arity = "1", paramLabel = "CONTINENT_ID",
             description = "Enter the continent ID to remove")
     int[] d_removeIds;
-    @Spec CommandSpec d_spec; // injected by picocli
+    @Spec
+    CommandSpec d_spec; // injected by picocli
 
     public Integer call() {
         validate();
-        if(d_addArray != null) {
-            for(int i = 0; i< d_addArray.length; i+=2) {
+        if (d_addArray != null) {
+            for (int i = 0; i < d_addArray.length; i += 2) {
                 int l_continent_id = d_addArray[i];
-                int l_continent_value = d_addArray[i+1];
-                System.out.println("Adding Continent:" + d_addArray[i] + " value:" + d_addArray[i+1]);
+                int l_continent_value = d_addArray[i + 1];
+                System.out.println("Adding Continent:" + d_addArray[i] + " value:" + d_addArray[i + 1]);
 //                CliParser.GameMap.addContinent(add_array[i], add_array[i+1]);
             }
         }
-        if(d_removeIds != null) {
-            for(int i = 0; i< d_removeIds.length; i++){
+        if (d_removeIds != null) {
+            for (int i = 0; i < d_removeIds.length; i++) {
                 System.out.println("Removing contient: " + d_removeIds[i]);
 //                CliParser.GameMap.removeContinent(remove_ids[i]);
             }
@@ -57,27 +58,28 @@ class EditContinent implements Callable<Integer>{
 }
 
 @Command(name = "editcountry")
-class EditCountry implements Callable<Integer>{
-    @Option(names = "-add", arity = "2", required=true,
+class EditCountry implements Callable<Integer> {
+    @Option(names = "-add", arity = "2", required = true,
             description = "Enter country ID and continent ID respectively")
     int[] d_addIds;
-    @Option(names = "-remove", arity = "1",required = true,
+    @Option(names = "-remove", arity = "1", required = true,
             description = "Enter the country ID to remove")
     int[] d_removeIds;
-    @Spec CommandSpec d_spec; // injected by picocli
+    @Spec
+    CommandSpec d_spec; // injected by picocli
 
     public Integer call() {
         validate();
-        if(d_addIds != null) {
-            for(int i = 0; i< d_addIds.length; i+=2) {
+        if (d_addIds != null) {
+            for (int i = 0; i < d_addIds.length; i += 2) {
                 int l_countryId = d_addIds[i];
-                int l_continentId = d_addIds[i+1];
-                System.out.println("Adding country: Continent:" + d_addIds[i] + " Country:" + d_addIds[i+1]);
+                int l_continentId = d_addIds[i + 1];
+                System.out.println("Adding country: Continent:" + d_addIds[i] + " Country:" + d_addIds[i + 1]);
 //                CliParser.GameMap.addCountry(d_add_ids[i], d_add_ids[i+1]);
             }
         }
-        if(d_removeIds != null) {
-            for(int i = 0; i< d_removeIds.length; i++){
+        if (d_removeIds != null) {
+            for (int i = 0; i < d_removeIds.length; i++) {
                 System.out.println("Removing country id:" + d_removeIds[i]);
 //                CliParser.GameMap.removeCountry(d_remove_ids[i]);
             }
@@ -99,7 +101,7 @@ class EditCountry implements Callable<Integer>{
 }
 
 @Command(name = "editneighbor")
-class EditNeighbor implements Callable<Integer>{
+class EditNeighbor implements Callable<Integer> {
 
 
     @Option(names = "-add", arity = "2",
@@ -110,19 +112,20 @@ class EditNeighbor implements Callable<Integer>{
             description = "Enter the country ID and neighbour country ID to remove",
             required = true)
     int[] d_remove_ids;
-    @Spec CommandSpec d_spec; // injected by picocli
+    @Spec
+    CommandSpec d_spec; // injected by picocli
 
     public Integer call() {
         validate();
-        if(d_add_ids != null) {
-            for(int i = 0; i< d_add_ids.length; i+=2) {
-                System.out.println("Adding neighbor:" + d_add_ids[i] + " Country:" + d_add_ids[i+1]);
+        if (d_add_ids != null) {
+            for (int i = 0; i < d_add_ids.length; i += 2) {
+                System.out.println("Adding neighbor:" + d_add_ids[i] + " Country:" + d_add_ids[i + 1]);
 //                CliParser.GameMap.addNeighbor(d_add_ids[i], d_add_ids[i+1]);
             }
         }
-        if(d_remove_ids != null) {
-            for(int i = 0; i< d_remove_ids.length; i+=2){
-                System.out.println("Removing neighbor: country:" + d_remove_ids[i] + " Country:" + d_remove_ids[i+1]);
+        if (d_remove_ids != null) {
+            for (int i = 0; i < d_remove_ids.length; i += 2) {
+                System.out.println("Removing neighbor: country:" + d_remove_ids[i] + " Country:" + d_remove_ids[i + 1]);
 //                CliParser.GameMap.removeNeighbor(d_remove_ids[i], d_remove_ids[i+1]);
             }
         }
@@ -143,7 +146,7 @@ class EditNeighbor implements Callable<Integer>{
 }
 
 @Command(name = "showmap")
-class ShowMap implements Callable<Integer>{
+class ShowMap implements Callable<Integer> {
     public Integer call() {
 //        CliParser.GameMap.showMap();
         System.out.println("Showing map");
@@ -152,7 +155,7 @@ class ShowMap implements Callable<Integer>{
 }
 
 @Command(name = "savemap")
-class SaveMap implements Callable<Integer>{
+class SaveMap implements Callable<Integer> {
     public Integer call() {
 //        CliParser.GameMap.saveMap();
         System.out.println("Saving map");
@@ -161,7 +164,7 @@ class SaveMap implements Callable<Integer>{
 }
 
 @Command(name = "editmap")
-class EditMap implements Callable<Integer>{
+class EditMap implements Callable<Integer> {
     @Parameters(index = "0",
             paramLabel = "Filename",
             description = "Filename of the map")
@@ -176,7 +179,7 @@ class EditMap implements Callable<Integer>{
 
 
 @Command(name = "validatemap")
-class ValidateMap implements Callable<Integer>{
+class ValidateMap implements Callable<Integer> {
     public Integer call() {
 //        CliParser.GameMap.validateMap();
         System.out.println("Validate map");
@@ -185,7 +188,7 @@ class ValidateMap implements Callable<Integer>{
 }
 
 @Command(name = "loadmap")
-class LoadMap implements Callable<Integer>{
+class LoadMap implements Callable<Integer> {
     @Parameters(index = "0",
             paramLabel = "Filename of the map",
             description = "Filename of the map")
@@ -203,7 +206,7 @@ class LoadMap implements Callable<Integer>{
 }
 
 @Command(name = "gameplayer")
-class GamePlayer implements Callable<Integer>{
+class GamePlayer implements Callable<Integer> {
     @Option(names = "-add",
             description = "Enter player name",
             required = true)
@@ -212,19 +215,20 @@ class GamePlayer implements Callable<Integer>{
             description = "Enter the player name",
             required = true)
     String[] d_remove_names;
-    @Spec CommandSpec d_spec; // injected by picocli
+    @Spec
+    CommandSpec d_spec; // injected by picocli
 
     public Integer call() {
         validate();
         //TODO: Functionality to add a new player with id
-        if(d_add_names != null) {
+        if (d_add_names != null) {
             for (String dAddName : d_add_names) {
 //                CliParser.GamePlayers.add(new Player());
                 System.out.println("Adding player id: " + dAddName);
             }
         }
         //TODO: Functionality to remove a player using id
-        if(d_remove_names != null) {
+        if (d_remove_names != null) {
             for (String dRemoveName : d_remove_names) {
 //                CliParser.GamePlayers.remove(d_remove_ids[i]);
                 System.out.println("Removing player id: " + dRemoveName);
@@ -249,7 +253,7 @@ class GamePlayer implements Callable<Integer>{
 
 //TODO: Decide on how to approach
 @Command(name = "assigncountries")
-class AssignCountries implements Callable<Integer>{
+class AssignCountries implements Callable<Integer> {
     public Integer call() {
         GameEngine.get_instance().assignCountries();
         return 0;
@@ -258,14 +262,14 @@ class AssignCountries implements Callable<Integer>{
 
 //TODO: Decide on how to implement
 @Command(name = "deploy")
-class Deploy implements Callable<Integer>{
+class Deploy implements Callable<Integer> {
     @Parameters(index = "0", description = "country ID")
     int d_countryID;
     @Parameters(index = "1", description = "Number of army units")
     int d_num;
 
     public Integer call() {
-        System.out.println("Deploy command");
+        GameEngine.get_instance().deployArmy(d_countryID, d_num);
         return 0;
     }
 }

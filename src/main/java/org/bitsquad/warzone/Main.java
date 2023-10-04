603074afa71ae52daf58b5384fc59845baddae19
amansahu278 <a.man.cubes24@gmail.com>
2023-10-04 01:29:16 -0400

    Moved the game code to Main class

--- src/main/java/org/bitsquad/warzone/Main.java
+++ src/main/java/org/bitsquad/warzone/Main.java
@@ -1,10 +1,28 @@
 package org.bitsquad.warzone;
 
+import org.bitsquad.warzone.cli.CliParser;
+
+import java.util.Scanner;
+
 /**
  * Main Class
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+
+        System.out.println("Hello and welcome to WARZONE!");
+        CliParser parser = new CliParser();
+        Scanner scanner = new Scanner(System.in);
+
+        String ip;
+        while (true) {
+            System.out.print(">");
+            ip = scanner.nextLine();
+            try {
+                parser.parseCommandString(ip);
+            } catch (Exception e) {
+                System.err.println(e.getMessage());
+            }
+        }
     }
 }

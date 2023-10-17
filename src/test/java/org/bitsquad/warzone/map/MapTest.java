3d1c50eefe9ee749120f080cae988ee64e5a427f
amansahu278 <a.man.cubes24@gmail.com>
2023-10-17 17:37:35 -0400

    Renamed map to d_map

--- src/test/java/org/bitsquad/warzone/map/MapTest.java
+++ src/test/java/org/bitsquad/warzone/map/MapTest.java
@@ -2,7 +2,6 @@ package org.bitsquad.warzone.map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.bitsquad.warzone.country.Country;
 import org.junit.jupiter.api.*;
 
 import java.io.*;
@@ -14,15 +13,15 @@ import java.nio.file.Paths;
  * This tests the functionality of Map class.
  */
 class MapTest {
-	private Map map;
+	private Map d_map;
 
 	@BeforeEach
 	public void setUp() {
-		map = new Map();
+		d_map = new Map();
 	}
 
 	@AfterEach
-	public void destroy(){ map = null; }
+	public void destroy(){ d_map = null; }
 
 
 	@AfterAll
@@ -30,10 +29,10 @@ class MapTest {
 		// Deleting temp created files
 		try {
 			// Delete the file
-			String filenames[] = {"tempMap.map", "savedMap.map", "testMap.map"};
+			String l_filenames[] = {"tempMap.map", "savedMap.map", "testMap.map"};
 
-			for(String f_name: filenames){
-				Files.deleteIfExists(Paths.get(f_name));
+			for(String l_fname: l_filenames){
+				Files.deleteIfExists(Paths.get(l_fname));
 			}
 		}
 		// If the input directory is not empty
@@ -55,10 +54,10 @@ class MapTest {
 	 */
 	@Test
 	public void testAddContinent() {
-		map.addContinent(1, 5);
-		assertNotNull(map.getContinents().get(1));
-		assertNull(map.getContinents().get(2));
-		assertEquals(5, map.getContinents().get(1).getValue());
+		d_map.addContinent(1, 5);
+		assertNotNull(d_map.getContinents().get(1));
+		assertNull(d_map.getContinents().get(2));
+		assertEquals(5, d_map.getContinents().get(1).getValue());
 	}
 
 	/**
@@ -66,14 +65,14 @@ class MapTest {
 	 */
 	@Test
 	public void testAddCountry() {
-		map.addContinent(1, 5);
-		assertTrue(map.addCountry(1, 1));
-		assertFalse(map.addCountry(1, 2));
-		assertNotNull(map.getContinents().get(1).getCountries().get(1));
+		d_map.addContinent(1, 5);
+		assertTrue(d_map.addCountry(1, 1));
+		assertFalse(d_map.addCountry(1, 2));
+		assertNotNull(d_map.getContinents().get(1).getCountries().get(1));
 
-		map.addContinent(2, 4);
-		assertFalse(map.addCountry(1,2));
-		assertNull(map.getContinents().get(2).getCountries().get(1));
+		d_map.addContinent(2, 4);
+		assertFalse(d_map.addCountry(1,2));
+		assertNull(d_map.getContinents().get(2).getCountries().get(1));
 	}
 
 	/**
@@ -81,9 +80,9 @@ class MapTest {
 	 */
 	@Test
 	public void testRemoveContinent() {
-		map.addContinent(1, 5);
-		assertTrue(map.removeContinent(1));
-		assertNull(map.getContinents().get(1));
+		d_map.addContinent(1, 5);
+		assertTrue(d_map.removeContinent(1));
+		assertNull(d_map.getContinents().get(1));
 	}
 
 	/**
@@ -91,16 +90,16 @@ class MapTest {
 	 */
 	@Test
 	public void testAddNeighbor() {
-		map.addContinent(1, 5);
-		map.addContinent(2, 5);
-		map.addCountry(1, 1);
-		map.addCountry(2, 2);
-		map.addCountry(3, 1);
-		assertTrue(map.addNeighbor(1, 2));
-		assertTrue(map.addNeighbor(1, 3));
-		assertTrue(map.addNeighbor(3, 1));
-		assertTrue(map.getContinents().get(1).getCountries().get(1).getNeighbors().contains(2));
-		assertTrue(map.getContinents().get(2).getCountries().get(2).getNeighbors().contains(1));
+		d_map.addContinent(1, 5);
+		d_map.addContinent(2, 5);
+		d_map.addCountry(1, 1);
+		d_map.addCountry(2, 2);
+		d_map.addCountry(3, 1);
+		assertTrue(d_map.addNeighbor(1, 2));
+		assertTrue(d_map.addNeighbor(1, 3));
+		assertTrue(d_map.addNeighbor(3, 1));
+		assertTrue(d_map.getContinents().get(1).getCountries().get(1).getNeighbors().contains(2));
+		assertTrue(d_map.getContinents().get(2).getCountries().get(2).getNeighbors().contains(1));
 	}
 
 
@@ -109,12 +108,12 @@ class MapTest {
 	 */
 	@Test
 	public void testRemoveNeighbor() {
-		map.addContinent(1, 5);
-		map.addCountry(1, 1);
-		map.addCountry(2, 1);
-		map.addNeighbor(1, 2);
-		map.removeNeighbor(1, 2);
-		assertFalse(map.getContinents().get(1).getCountries().get(1).getNeighbors().contains(2));
+		d_map.addContinent(1, 5);
+		d_map.addCountry(1, 1);
+		d_map.addCountry(2, 1);
+		d_map.addNeighbor(1, 2);
+		d_map.removeNeighbor(1, 2);
+		assertFalse(d_map.getContinents().get(1).getCountries().get(1).getNeighbors().contains(2));
 	}
 
 	/**
@@ -123,23 +122,23 @@ class MapTest {
 	 */
 	@Test
 	public void testLoadMap() throws IOException {
-		String mapData = "[continents]\n1 5\n2 3\n\n[countries]\n1 Country_1 1\n2 Country_2 1\n3 Country_3 2\n4 Country_4 2\n\n[neighbors]\n1 2\n2 1\n3 4\n4 3\n";
-		File file = new File("testMap.map");
-		BufferedWriter writer = new BufferedWriter(new FileWriter(file));
-		writer.write(mapData);
-		writer.flush();
-		writer.close();
-		map.loadMap("testMap.map");
-		assertNotNull(map.getContinents().get(1));
-		assertNotNull(map.getContinents().get(2));
-		assertNotNull(map.getContinents().get(1).getCountries().get(1));
-		assertNotNull(map.getContinents().get(1).getCountries().get(2));
-		assertNotNull(map.getContinents().get(2).getCountries().get(3));
-		assertNotNull(map.getContinents().get(2).getCountries().get(4));
-		assertTrue(map.getContinents().get(1).getCountries().get(1).getNeighbors().contains(2));
-		assertTrue(map.getContinents().get(1).getCountries().get(2).getNeighbors().contains(1));
-		assertTrue(map.getContinents().get(2).getCountries().get(3).getNeighbors().contains(4));
-		assertTrue(map.getContinents().get(2).getCountries().get(4).getNeighbors().contains(3));
+		String l_mapData = "[continents]\n1 5\n2 3\n\n[countries]\n1 Country_1 1\n2 Country_2 1\n3 Country_3 2\n4 Country_4 2\n\n[neighbors]\n1 2\n2 1\n3 4\n4 3\n";
+		File l_file = new File("testMap.map");
+		BufferedWriter l_writer = new BufferedWriter(new FileWriter(l_file));
+		l_writer.write(l_mapData);
+		l_writer.flush();
+		l_writer.close();
+		d_map.loadMap("testMap.map");
+		assertNotNull(d_map.getContinents().get(1));
+		assertNotNull(d_map.getContinents().get(2));
+		assertNotNull(d_map.getContinents().get(1).getCountries().get(1));
+		assertNotNull(d_map.getContinents().get(1).getCountries().get(2));
+		assertNotNull(d_map.getContinents().get(2).getCountries().get(3));
+		assertNotNull(d_map.getContinents().get(2).getCountries().get(4));
+		assertTrue(d_map.getContinents().get(1).getCountries().get(1).getNeighbors().contains(2));
+		assertTrue(d_map.getContinents().get(1).getCountries().get(2).getNeighbors().contains(1));
+		assertTrue(d_map.getContinents().get(2).getCountries().get(3).getNeighbors().contains(4));
+		assertTrue(d_map.getContinents().get(2).getCountries().get(4).getNeighbors().contains(3));
 	}
 
 	/**
@@ -148,31 +147,31 @@ class MapTest {
 	 */
 	@Test
 	public void testSaveMap() throws IOException {
-		map.addContinent(1, 5);
-		map.addContinent(2, 3);
-		map.addCountry(1, 1);
-		map.addCountry(2, 1);
-		map.addCountry(3, 2);
-		map.addCountry(4, 2);
-		map.addNeighbor(1, 2);
-		map.addNeighbor(2, 1);
-		map.addNeighbor(2, 4);
-		map.addNeighbor(4, 3);
+		d_map.addContinent(1, 5);
+		d_map.addContinent(2, 3);
+		d_map.addCountry(1, 1);
+		d_map.addCountry(2, 1);
+		d_map.addCountry(3, 2);
+		d_map.addCountry(4, 2);
+		d_map.addNeighbor(1, 2);
+		d_map.addNeighbor(2, 1);
+		d_map.addNeighbor(2, 4);
+		d_map.addNeighbor(4, 3);
 		try {
-			map.saveMap("savedMap");
+			d_map.saveMap("savedMap");
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
-		File file = new File("savedMap.map");
-		assertTrue(file.exists());
-		StringBuilder content = new StringBuilder();
-		BufferedReader l_bufferedReader = new BufferedReader(new FileReader(file));
-		String line;
-		while ((line = l_bufferedReader.readLine()) != null) {
-			content.append(line).append("\n");
+		File l_file = new File("savedMap.map");
+		assertTrue(l_file.exists());
+		StringBuilder l_content = new StringBuilder();
+		BufferedReader l_bufferedReader = new BufferedReader(new FileReader(l_file));
+		String l_line;
+		while ((l_line = l_bufferedReader.readLine()) != null) {
+			l_content.append(l_line).append("\n");
 		}
-		String expected = "[continents]\n1 5\n2 3\n\n[countries]\n1 Country_1 1\n2 Country_2 1\n3 Country_3 2\n4 Country_4 2\n\n[neighbors]\n1 2\n2 1 4\n3 4\n4 2 3\n";
-		assertEquals(expected, content.toString());
+		String l_expected = "[continents]\n1 5\n2 3\n\n[countries]\n1 Country_1 1\n2 Country_2 1\n3 Country_3 2\n4 Country_4 2\n\n[neighbors]\n1 2\n2 1 4\n3 4\n4 2 3\n";
+		assertEquals(l_expected, l_content.toString());
 	}
 
 	/**
@@ -181,20 +180,20 @@ class MapTest {
 	 */
 	@Test
 	public void testEditMap() throws IOException{
-		File file = new File("tempMap.map");
-		if(file.exists())
-			assertTrue(map.loadMap("tempMap.map"));
+		File l_file = new File("tempMap.map");
+		if(l_file.exists())
+			assertTrue(d_map.loadMap("tempMap.map"));
 		else{
-			map.editMap("tempMap.map");
-			assertTrue(file.exists());
-			StringBuilder content = new StringBuilder();
-			BufferedReader reader = new BufferedReader(new FileReader(file));
-			String line;
-			while ((line = reader.readLine()) != null) {
-				content.append(line).append("\n");
+			d_map.editMap("tempMap.map");
+			assertTrue(l_file.exists());
+			StringBuilder l_content = new StringBuilder();
+			BufferedReader l_reader = new BufferedReader(new FileReader(l_file));
+			String l_line;
+			while ((l_line = l_reader.readLine()) != null) {
+				l_content.append(l_line).append("\n");
 			}
-			String expected = "[continents]\n\n[countries]\n\n[neighbors]\n";
-			assertEquals(expected, content.toString());
+			String l_expected = "[continents]\n\n[countries]\n\n[neighbors]\n";
+			assertEquals(l_expected, l_content.toString());
 		}
 	}
 
@@ -203,26 +202,26 @@ class MapTest {
 	 */
 	@Test
 	public void testIsContinentSubgraph(){
-		map.addContinent(1, 5);
-		map.addCountry(1, 1);
-		map.addCountry(2, 1);
-		map.addCountry(3, 1);
-		map.addContinent(2, 5);
-		map.addCountry(4, 2);
-		map.addCountry(5, 2);
-		map.addContinent(3, 5);
-		map.addCountry(6, 3);
-		map.addCountry(7, 3);
+		d_map.addContinent(1, 5);
+		d_map.addCountry(1, 1);
+		d_map.addCountry(2, 1);
+		d_map.addCountry(3, 1);
+		d_map.addContinent(2, 5);
+		d_map.addCountry(4, 2);
+		d_map.addCountry(5, 2);
+		d_map.addContinent(3, 5);
+		d_map.addCountry(6, 3);
+		d_map.addCountry(7, 3);
 
-		map.addNeighbor(1, 2);
-		map.addNeighbor(2, 3);
-		map.addNeighbor(2, 4);
-		map.addNeighbor(4, 5);
-		map.addNeighbor(6, 5);
+		d_map.addNeighbor(1, 2);
+		d_map.addNeighbor(2, 3);
+		d_map.addNeighbor(2, 4);
+		d_map.addNeighbor(4, 5);
+		d_map.addNeighbor(6, 5);
 
-		assertTrue(map.isContinentSubgraph(1));
-		assertTrue(map.isContinentSubgraph(2));
-		assertFalse(map.isContinentSubgraph(3));
+		assertTrue(d_map.isContinentSubgraph(1));
+		assertTrue(d_map.isContinentSubgraph(2));
+		assertFalse(d_map.isContinentSubgraph(3));
 	}
 
 	/**
@@ -230,37 +229,37 @@ class MapTest {
 	 */
 	@Test
 	public void testValidateMap(){
-		map.addContinent(1, 5);
-		map.addCountry(1, 1);
-		map.addCountry(2, 1);
-		map.addCountry(3, 1);
-		map.addContinent(2, 5);
-		map.addCountry(4, 2);
-		map.addCountry(5, 2);
-		map.addContinent(3, 5);
-		map.addCountry(6, 3);
-		map.addCountry(7, 3);
+		d_map.addContinent(1, 5);
+		d_map.addCountry(1, 1);
+		d_map.addCountry(2, 1);
+		d_map.addCountry(3, 1);
+		d_map.addContinent(2, 5);
+		d_map.addCountry(4, 2);
+		d_map.addCountry(5, 2);
+		d_map.addContinent(3, 5);
+		d_map.addCountry(6, 3);
+		d_map.addCountry(7, 3);
 		// Graph not connected
-		assertFalse(map.validateMap());
+		assertFalse(d_map.validateMap());
 
-		map.addNeighbor(1, 2);
-		map.addNeighbor(2, 3);
-		map.addNeighbor(2, 4);
-		map.addNeighbor(4, 5);
-		map.addNeighbor(6, 5);
-		map.addNeighbor(7, 6);
-		assertTrue(map.validateMap());
+		d_map.addNeighbor(1, 2);
+		d_map.addNeighbor(2, 3);
+		d_map.addNeighbor(2, 4);
+		d_map.addNeighbor(4, 5);
+		d_map.addNeighbor(6, 5);
+		d_map.addNeighbor(7, 6);
+		assertTrue(d_map.validateMap());
 
-		map.removeNeighbor(7, 6);
+		d_map.removeNeighbor(7, 6);
 		//Country 7 not connected
-		assertFalse(map.validateMap());
+		assertFalse(d_map.validateMap());
 
-		map.removeContinent(3);
-		map.addContinent(3, 5);
-		map.addCountry(6, 3);
-		map.addCountry(7, 3);
+		d_map.removeContinent(3);
+		d_map.addContinent(3, 5);
+		d_map.addCountry(6, 3);
+		d_map.addCountry(7, 3);
 
 		// Continent 3 is not a valid subgraph (not connected)
-		assertFalse(map.validateMap());
+		assertFalse(d_map.validateMap());
 	}
 }

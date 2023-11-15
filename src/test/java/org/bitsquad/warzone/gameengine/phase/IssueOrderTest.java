a099e74d3fbc680223b95ce4dcd160852c899a86
xsrujanax <srujana.guttula@outlook.com>
2023-11-15 14:40:20 -0500

    fixed OrderPreDeploy and PostDeploy TestCases

---
+++ src/test/java/org/bitsquad/warzone/gameengine/phase/IssueOrderTest.java
@@ -0,0 +1,12 @@
+package org.bitsquad.warzone.gameengine.phase;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * This tests the functionality of Issue Order class.
+ */
+public class IssueOrderTest {
+    @Test
+    void test() {
+    }
+}
---
+++ src/test/java/org/bitsquad/warzone/gameengine/phase/OrderExecutionTest.java
@@ -0,0 +1,12 @@
+package org.bitsquad.warzone.gameengine.phase;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * This tests the functionality of Order Execution class.
+ */
+public class OrderExecutionTest {
+    @Test
+    void test() {
+    }
+}
